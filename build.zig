f945a6a2daa2d5b194763b95db21dce5ae209aba
Simon Hartcher <simon@simonhartcher.com>
2025-06-27 11:01:58 +1000

    build: add bbcodez module

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.createModule(.{
+    const lib_mod = b.addModule("bbcodez", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
