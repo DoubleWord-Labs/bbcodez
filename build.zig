bd6151485789e9f68b7674a25fe12e971e7804a8
Simon Hartcher <simon@simonhartcher.com>
2025-07-11 23:25:37 +1000

    feat: add bbcodez executable for processing in the terminal

--- build.zig
+++ build.zig
@@ -24,9 +24,6 @@ pub fn build(b: *std.Build) void {
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-
     const install_docs = b.addInstallDirectory(.{
         .source_dir = lib.getEmittedDocs(),
         .install_dir = .prefix,
@@ -36,6 +33,45 @@ pub fn build(b: *std.Build) void {
     const docs_step = b.step("docs", "Generate documentation");
     docs_step.dependOn(&install_docs.step);
     install.step.dependOn(docs_step);
+
+    const cli_dep = b.dependency("cli", .{});
+    const cli_mod = cli_dep.module("cli");
+
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe_mod.addImport("cli", cli_mod);
+    exe_mod.addImport("lib", lib_mod);
+
+    const exe = b.addExecutable(.{
+        .name = "bbcodez",
+        .root_module = exe_mod,
+    });
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const exe_unit_tests = b.addTest(.{
+        .root_module = exe_mod,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    run_exe_unit_tests.step.dependOn(b.getInstallStep());
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_exe_unit_tests.step);
 }
 
 const std = @import("std");
---
+++ src/main.zig
@@ -0,0 +1,99 @@
+pub const std_options: Options = .{
+    .log_level = Level.err,
+};
+
+var config = struct {
+    input: ?[]const u8 = null,
+    output: ?[]const u8 = null,
+}{};
+
+const StreamSource = enum {
+    file,
+    stdin,
+};
+
+const StreamDestination = enum {
+    file,
+    stdout,
+};
+
+pub fn main() !void {
+    var r = try cli.AppRunner.init(std.heap.page_allocator);
+
+    const app = cli.App{
+        .command = cli.Command{
+            .name = "bbcodez",
+            .options = try r.allocOptions(&.{
+                .{
+                    .long_name = "input",
+                    .help = "input file",
+                    .value_ref = r.mkRef(&config.input),
+                },
+                .{
+                    .long_name = "output",
+                    .help = "output file",
+                    .value_ref = r.mkRef(&config.output),
+                },
+            }),
+            .target = cli.CommandTarget{
+                .action = cli.CommandAction{ .exec = processConfig },
+            },
+        },
+    };
+    return r.run(&app);
+}
+
+fn processConfig() !void {
+    var input_file: File = undefined;
+    defer input_file.close();
+
+    var output_file: File = undefined;
+    defer output_file.close();
+
+    const input_source: StreamSource = if (config.input == null) .stdin else .file;
+    const output_source: StreamDestination = if (config.output == null) .stdout else .file;
+
+    switch (input_source) {
+        .file => {
+            input_file = try cwd().openFile(config.input.?, .{});
+        },
+        .stdin => {
+            input_file = std.io.getStdIn();
+        },
+    }
+
+    switch (output_source) {
+        .file => {
+            output_file = try cwd().openFile(config.output.?, .{});
+        },
+        .stdout => {
+            output_file = std.io.getStdOut();
+        },
+    }
+
+    const reader = input_file.reader().any();
+    const writer = output_file.writer().any();
+
+    var arena = ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const document = try lib.load(allocator, reader, .{});
+    defer document.deinit();
+
+    try renderDocument(allocator, document, writer, .{});
+}
+
+const cwd = std.fs.cwd;
+const renderDocument = lib.fmt.md.renderDocument;
+
+const File = std.fs.File;
+const AnyReader = std.io.AnyReader;
+const AnyWriter = std.io.AnyWriter;
+const ArenaAllocator = std.heap.ArenaAllocator;
+const Level = std.log.Level;
+const Options = std.Options;
+
+const std = @import("std");
+const cli = @import("cli");
+const lib = @import("lib");
